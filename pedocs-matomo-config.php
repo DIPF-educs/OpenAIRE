dbbe4286674a6ccca68dbe290456f262a2294e30
Henning Hinze <hinze@dipf.de>
2020-01-31 13:27:44 +0100

    Neue Datei hochladen

---
+++ pedocs-matomo-config.php
@@ -0,0 +1,13 @@
+<?php
+$config=array(
+  'log_start_jahr'  => "2020",
+  'log_start_monat' => "01",
+  'log_start_tag'   => "23",
+
+//   'in_folder'      => '/data/httpd_logs/all_non_anon/pedocs',
+  'in_folder'      => '/home/pedocs_2/nicht_in_git/test_logs',
+  'data_folder'    => '/home/pedocs_2/lib/OpenAireStatistics',
+  'current-log_subfolder' => '/logs/aktuell',
+  'success-log_subfolder' => '/logs/erfolg',
+  'error-log_subfolder'   => '/logs/fehler',
+);
