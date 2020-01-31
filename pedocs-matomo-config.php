ce1d9f860f514ee4e52d0c41b86ff05dd89be3bc
Henning Hinze <hinze@dipf.de>
2020-01-31 13:28:48 +0100

    Update pedocs-matomo-config.php

--- pedocs-matomo-config.php
+++ pedocs-matomo-config.php
@@ -4,8 +4,7 @@ $config=array(
   'log_start_monat' => "01",
   'log_start_tag'   => "23",
 
-//   'in_folder'      => '/data/httpd_logs/all_non_anon/pedocs',
-  'in_folder'      => '/home/pedocs_2/nicht_in_git/test_logs',
+  'in_folder'      => '/data/httpd_logs/all_non_anon/pedocs',
   'data_folder'    => '/home/pedocs_2/lib/OpenAireStatistics',
   'current-log_subfolder' => '/logs/aktuell',
   'success-log_subfolder' => '/logs/erfolg',
